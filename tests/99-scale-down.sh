83d853da39c73dfec0f0a14a1e1474028570db3c
Morgan Haywood <morgan.haywood@myob.com>
2020-06-26 15:10:15 +1000

    wtf is hanging?

--- tests/99-scale-down.sh
+++ tests/99-scale-down.sh
@@ -5,17 +5,23 @@ die() { echo "${1:-ded}" >&2; exit "${2:-1}"; }
 
 id=$(/opt/aws/bin/ec2-metadata --instance-id | cut -d " " -f 2) \
   || die "can't get id"
+echo "got id"
 ec2=$( aws ec2 describe-instances --instance-ids "$id") \
   || die "can't get instance info"
+echo "got instance"
 q=$( jq -r '.Reservations[].Instances[].Tags[] | select ( .Key | contains ("BuildkiteQueue")) | .Value' ) <<<"$ec2" \
   || die "can't get queue name"
+echo "got queue name"
 sdp=$(aws ssm get-parameter --name "/bk/$q/ScaleDownPeriod" --query Parameter.Value --output text) \
   || die "can't get scale down period"
+echo "got scale down"
 asg=$( jq -r '.Reservations[].Instances[].Tags[] | select ( .Key | contains ("aws:autoscaling:groupName")) | .Value' ) <<<"$ec2" \
   || die "can't get asg name"
+echo "got asg name"
 dc=$( aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name "$asg" \
   | jq -r '.AutoScalingGroups[].DesiredCapacity' ) \
   || die "can't get desired capacity"
+echo "got desired capacity"
 
 echo "~~~ sleeping for 60 seconds"
 
