3b6591b16cb83a93fa324b6de5d63561ee0bc11f
Morgan Haywood <morgan.haywood@myob.com>
2020-06-29 10:13:51 +1000

    more output

--- tests/99-scale-down.sh
+++ tests/99-scale-down.sh
@@ -5,19 +5,19 @@ die() { echo "${1:-ded}" >&2; exit "${2:-1}"; }
 
 id=$(/opt/aws/bin/ec2-metadata --instance-id | cut -d " " -f 2) \
   || die "can't get id"
-echo "got id"
+echo "got id: $id"
 ec2=$( aws ec2 describe-instances --instance-ids "$id") \
   || die "can't get instance info"
-echo "got instance"
+echo "got instance info"
 q=$( jq -r '.Reservations[].Instances[].Tags[] | select ( .Key | contains ("BuildkiteQueue")) | .Value' <<<"$ec2" ) \
   || die "can't get queue name"
-echo "got queue name"
+echo "got queue name: $q"
 sdp=$(aws ssm get-parameter --name "/bk/$q/ScaleDownPeriod" --query Parameter.Value --output text) \
   || die "can't get scale down period"
-echo "got scale down"
+echo "got scale down: $sdp"
 asg=$( jq -r '.Reservations[].Instances[].Tags[] | select ( .Key | contains ("aws:autoscaling:groupName")) | .Value' <<<"$ec2" ) \
   || die "can't get asg name"
-echo "got asg name"
+echo "got asg name: $asg"
 
 echo "~~~ sleeping for $((sdp*3)) seconds"
 
