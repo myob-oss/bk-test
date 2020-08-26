7393d7f82565c080c33039639e46de24544e7817
Morgan Haywood <morgan.haywood@myob.com>
2020-08-26 10:27:17 +1000

    rm tests relying on adding secret

--- tests/02-setup-secret.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-env=$(mktemp)
-cleanup() { rm "$env"; }
-trap cleanup EXIT
-
-echo "export SECRET_QUOTE='We are stuck with technology when what we really want is just stuff that works.'" > "$env"
-
-aws s3 cp --acl private --sse aws:kms "$env" s3://"$BUILDKITE_SECRETS_BUCKET"/"$BUILDKITE_PIPELINE_SLUG"/env
--- tests/12-use-secret.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export
-
-echo "${SECRET_QUOTE:?secret not found}"
-echo "  - Douglas Adams"
-echo "secrets are working correctly!"
